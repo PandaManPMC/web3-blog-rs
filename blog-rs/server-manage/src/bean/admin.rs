76987462cec57b77e8e7d76116c93c4996f8a4d1
PMC <pandamancoin@gmail.com>
2024-06-05 15:51:57 +0500

    1

--- blog-rs/server-manage/src/bean/admin.rs
+++ blog-rs/server-manage/src/bean/admin.rs
@@ -5,7 +5,7 @@ use serde::Deserializer;
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 pub struct LoginIn {
     /// search用户名 【max:20】
-    #[serde(rename = "userName", deserialize_with = "check_length")]
+    #[serde(rename = "userName", deserialize_with = "check_length_user_name")]
     pub user_name: String,
     /// 密码 【max:64】
     #[serde(rename = "userPwd")]
@@ -16,15 +16,15 @@ pub struct LoginIn {
     pub google_auth_code: String,
 }
 
-fn check_length<'de, D>(deserializer: D) -> Result<String, D::Error>
+fn check_length_user_name<'de, D>(deserializer: D) -> Result<String, D::Error>
     where
         D: Deserializer<'de>,
 {
     let s: String = Deserialize::deserialize(deserializer)?;
-    if s.len() <= 10 {
+    if s.len() <= 20 {
         Ok(s)
     } else {
-        Err(de::Error::invalid_value(Unexpected::Str(&s), &"a string with at most 10 characters"))
+        Err(de::Error::invalid_value(Unexpected::Str(&s), &"a string with at most 20 characters"))
     }
 }
 
