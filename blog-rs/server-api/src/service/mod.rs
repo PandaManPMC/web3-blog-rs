0d97d9321ea8a73b3f8da4a9e8ea8fcf17254c5f
PMC <pandamancoin@gmail.com>
2024-06-13 16:26:42 +0500

    api

--- blog-rs/server-api/src/service/mod.rs
+++ blog-rs/server-api/src/service/mod.rs
@@ -2,20 +2,21 @@
 pub mod blog;
 
 
-use std::sync::RwLock;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 lazy_static::lazy_static! {
     /// DATA_SOURCE_KEY 数据源 key
-    static ref DATA_SOURCE_KEY: RwLock<String> = RwLock::new({
+    static ref DATA_SOURCE_KEY: Arc<RwLock<String>> = Arc::new(RwLock::new({
         String::from("mysql1")
-    });
+    }));
 }
 
-pub fn get_data_source_key() -> String {
-    return DATA_SOURCE_KEY.read().unwrap().to_string();
+pub async fn get_data_source_key() -> String {
+    return DATA_SOURCE_KEY.read().await.to_string();
 }
 
-pub fn set_date_source_key(key: String) {
-    let mut w = DATA_SOURCE_KEY.write().unwrap();
+pub async fn set_date_source_key(key: String) {
+    let mut w = DATA_SOURCE_KEY.write().await;
     *w = key;
 }
