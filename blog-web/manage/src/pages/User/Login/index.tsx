9c6bc69fe592b0fa9d3ee0a6124da5c1edeb8d99
PMC <pandamancoin@gmail.com>
2024-07-16 08:55:54 +0500

    test

--- blog-web/manage/src/pages/User/Login/index.tsx
+++ blog-web/manage/src/pages/User/Login/index.tsx
@@ -115,6 +115,7 @@ const Login: React.FC = () => {
   };
 
   const handleSubmit = async (values: API.LoginParams) => {
+    console.log(values);
     try {
       // 登录
       const msg = await login({ ...values, type });
@@ -199,8 +200,8 @@ const Login: React.FC = () => {
               {
                 key: 'mobile',
                 label: intl.formatMessage({
-                  id: 'pages.login.phoneLogin.tab',
-                  defaultMessage: '手机号登录',
+                  id: 'pages.login.metaMaskLogin.tab',
+                  defaultMessage: 'MetaMask 登录',
                 }),
               },
             ]}
--- blog-web/manage/src/pages/Welcome.tsx
+++ blog-web/manage/src/pages/Welcome.tsx
@@ -114,7 +114,7 @@ const Welcome: React.FC = () => {
               color: token.colorTextHeading,
             }}
           >
-            欢迎使用 Ant Design Pro
+            rust web3 blog
           </div>
           <p
             style={{
