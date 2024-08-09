3cad903d3b171d2f42fcc81c224943f68f53ad46
PMC <pandamancoin@gmail.com>
2024-08-09 16:03:28 +0500

    client_chakra:取消选择bug

---
+++ blog-web/client_chakra/app/home/home.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import React, {useEffect, useState} from 'react';
+import ArticleCard from "@/app/common/ArticleCard";
+import { Box, Button } from '@chakra-ui/react';
+import {useGetWrap} from "@/tool/http";
+
+let pageIndex = 1;
+const pageSize = 3;
+
+const Home = ({selectedQuery}: {selectedQuery: any}) => {
+    const envName = process.env.ENV_NAME;
+    console.log(envName);
+    const getWrap = useGetWrap();
+
+    const [articlesLst, setArticlesLst] = useState([]);
+    const [articlesLstLoading, setArticlesLstLoading] = useState(true);
+    const [noMore, setNoMore] = useState(false);
+
+    useEffect(() => {
+        console.log(66555)
+        pageIndex = 1;
+        getArticlesLst(pageIndex);
+    }, []);
+
+    useEffect(() => {
+        console.log("useEffect page.tsx selectedQuery=", selectedQuery)
+
+        if (undefined == selectedQuery) {
+            return;
+        }
+        if (0 > selectedQuery.idBlogLabel && 0 > selectedQuery.idBlogClasses) {
+            return;
+        }
+
+        pageIndex = 1;
+        setNoMore(false);
+        setArticlesLstLoading(true);
+        setArticlesLst([]);
+        getArticlesLst(pageIndex);
+    }, [selectedQuery]);
+
+    const getArticlesLst = async (pageIndex_: number) => {
+        console.log(pageIndex_);
+        let param = {pageIndex: pageIndex_, pageSize: pageSize};
+
+        if (selectedQuery) {
+            if (0 < selectedQuery.idBlogLabel) {
+                // @ts-ignore
+                param["idBlogLabel"] = selectedQuery.idBlogLabel;
+            }
+            if (0 < selectedQuery.idBlogClasses) {
+                // @ts-ignore
+                param["idBlogClasses"] = selectedQuery.idBlogClasses;
+            }
+        }
+
+        let data;
+        try {
+            data = await getWrap('/article/list', {params: param});
+        } catch (err) {
+            return;
+        }finally {
+            setArticlesLstLoading(false);
+        }
+        // console.log(data);
+
+        if (2000 != data.code) {
+            return;
+        }
+        console.log("data.len="+ data.data.length + ",articlesLst.len=" + articlesLst.length);
+
+        if (0 == data.data.length){
+            setNoMore(true);
+            return;
+        }
+
+        if (1 == pageIndex_) {
+            // @ts-ignore
+            setArticlesLst([...data.data]);
+        }else{
+            // @ts-ignore
+            setArticlesLst([...articlesLst, ...data.data]);
+        }
+    }
+
+    const loadMoreArticles = () => {
+        setArticlesLstLoading(true);
+        pageIndex += 1;
+        console.log(pageIndex);
+        getArticlesLst(pageIndex);
+    };
+
+    const handleArticleClick = (article: any) => {
+        console.log(article);
+    }
+
+    return (
+        <Box minHeight="100vh" maxWidth="100vh" display="flex" flexDirection="column">
+            <Box p={4}>
+                {articlesLst.map((article, index) => (
+                    <ArticleCard key={index} data={article} onClick={() => handleArticleClick(article)}/>
+                ))}
+                {noMore ? (
+                    <Box display="flex" justifyContent="center" mt={4}>
+                        没有更多
+                    </Box>
+                ) : (
+                    <Box display="flex" justifyContent="center" mt={4}>
+                        <Button onClick={loadMoreArticles} isLoading={articlesLstLoading} loadingText="加载中" colorScheme="gray" >
+                            加载更多
+                        </Button>
+                    </Box>
+                )}
+            </Box>
+        </Box>
+    );
+}
+
+export default Home;
+
--- blog-web/client_chakra/app/layout.tsx
+++ blog-web/client_chakra/app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 import Advertise from "./advertise/page";
 import Menu from "./menu/menu";
 import ScrollToTopButton from "@/app/common/ScrollToTopButton";
-import Home from "@/app/page";
+import Home from "@/app/home/home";
 // 自定义主题
 const theme = extendTheme({
     colors: {
@@ -44,12 +44,12 @@ const theme = extendTheme({
 });
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
 
-    const [selectedQuery, setSelectedQuery] = useState({idBlogLabel: 0, idBlogClasses: 0});
+    const [selectedQuery, setSelectedQuery] = useState({idBlogLabel: -1, idBlogClasses: -1});
 
     const handleMenuSelectedQuery = (item: {idBlogLabel: number, idBlogClasses: number}) => {
         setSelectedQuery({...selectedQuery, idBlogLabel: item.idBlogLabel, idBlogClasses: item.idBlogClasses});
@@ -58,45 +58,45 @@ export default function RootLayout({
     };
 
     return (
-      <html lang="zh">
-      <head>
-          <title>Next Template</title>
-          <meta name="viewport" content="initial-scale=1, width=device-width"/>
-      </head>
-      <body className={inter.className}>
-      <GlobalStateProvider>
-      <ChakraProvider theme={theme}>
-              <main>
-                  <div className="mobile-display-none">
-                      <Grid
-                          templateAreas={`"left center right"`}
-                          gridTemplateColumns={'1fr 3fr 1fr'}
-                          gap={4}
-                          p={4}
-                      >
-                          <Box gridArea="left" bg="gray.100" p={4} borderRadius="md">
-                              <Advertise></Advertise>
-                          </Box>
-                          <Box gridArea="center" bg="white" p={4} borderRadius="md" shadow="md">
-                              {/*{children}*/}
-                              <Home selectedQuery={selectedQuery}></Home>
-                          </Box>
-                          <Box gridArea="right" bg="gray.100" p={4} borderRadius="md">
-                              <Menu onMenuSelectedQuery={handleMenuSelectedQuery}></Menu>
-                          </Box>
-                      </Grid>
-                  </div>
-                  <div className="desktop-display-none">
-                      <Box gridArea="center" bg="white" p={4} borderRadius="md" shadow="md" >
-                          {children}
-                      </Box>
-                  </div>
-              </main>
-              <LargeWithNewsletter></LargeWithNewsletter>
-          <ScrollToTopButton />
-          </ChakraProvider>
-      </GlobalStateProvider>
-      </body>
-      </html>
-  );
+        <html lang="zh">
+        <head>
+            <title>Next Template</title>
+            <meta name="viewport" content="initial-scale=1, width=device-width"/>
+        </head>
+        <body className={inter.className}>
+        <GlobalStateProvider>
+            <ChakraProvider theme={theme}>
+                <main>
+                    <div className="mobile-display-none">
+                        <Grid
+                            templateAreas={`"left center right"`}
+                            gridTemplateColumns={'1fr 3fr 1fr'}
+                            gap={4}
+                            p={4}
+                        >
+                            <Box gridArea="left" bg="gray.100" p={4} borderRadius="md">
+                                <Advertise></Advertise>
+                            </Box>
+                            <Box gridArea="center" bg="white" p={4} borderRadius="md" shadow="md">
+                                {/*{children}*/}
+                                <Home selectedQuery={selectedQuery}></Home>
+                            </Box>
+                            <Box gridArea="right" bg="gray.100" p={4} borderRadius="md">
+                                <Menu onMenuSelectedQuery={handleMenuSelectedQuery}></Menu>
+                            </Box>
+                        </Grid>
+                    </div>
+                    <div className="desktop-display-none">
+                        <Box gridArea="center" bg="white" p={4} borderRadius="md" shadow="md" >
+                            {children}
+                        </Box>
+                    </div>
+                </main>
+                <LargeWithNewsletter></LargeWithNewsletter>
+                <ScrollToTopButton />
+            </ChakraProvider>
+        </GlobalStateProvider>
+        </body>
+        </html>
+    );
 }
